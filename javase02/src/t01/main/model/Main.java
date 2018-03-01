5198917bde4614ba66509b683689aa6266bdfa7e
MIXSTAYS <32819223+MIXSTAYS@users.noreply.github.com>
2018-03-01 08:54:36 +0300

    Removed commented strings.

--- javase02/src/t01/main/model/Main.java
+++ javase02/src/t01/main/model/Main.java
@@ -5,10 +5,8 @@ import static t01.main.model.PenColor.*;
 public class Main {
     public static void main(String[] args) {
         Pen black = new Pen("Pilot", GREEN, 0.461);
-//        black.setInkColorAndPenSize(BLACK, 0.8);
         Pen blue = new Pen("Parker");
         blue.setInkColorAndPenSize(BLUE, 0.8);
-//        System.out.println(black.equals(blue));
         System.out.println(black.toString());
         System.out.println(blue.toString());
     }
