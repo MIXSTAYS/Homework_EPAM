e34eba38af386abdaaa57e90fdd6ecafb3080488
MIXSTAYS <32819223+MIXSTAYS@users.noreply.github.com>
2018-04-05 13:55:26 +0300

    javase05 added

--- javase05/t01/test/java/PropertyReaderTest.java
+++ javase05/t01/test/java/PropertyReaderTest.java
@@ -1,2 +1,27 @@
+import exception.NoSuchKeyException;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class PropertyReaderTest {
+
+    @Test(expected = FileNotFoundException.class)
+    public void propertyReadShouldThrowFileNotFoundException() {
+        PropertyReader.propertyRead("t01\\test\\resources\\universalTEST.property");
+    }
+
+    @Test(expected = NoSuchKeyException.class)
+    public void propertyReadShouldThrowNoSuchKeyException() {
+        PropertyReader.propertyRead("t01\\test\\resources\\universal.property");
+        PropertyReader.propertyStringRead("5");
+    }
+
+    @Test
+    public void propertyReaderShouldReturnString() {
+        PropertyReader.propertyRead("t01\\test\\resources\\universal.property");
+        assertEquals("First", PropertyReader.propertyStringRead("1"));
+    }
 }
