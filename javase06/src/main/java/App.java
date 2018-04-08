3aae70f2195435b79040471434854374c27edde0
MIXSTAYS <32819223+MIXSTAYS@users.noreply.github.com>
2018-04-08 10:08:15 +0300

    javase06 edited

--- javase06/src/main/java/App.java
+++ javase06/src/main/java/App.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        PropertyReaderToMap.propertyRead("javase06\\src\\main\\java\\t02\\resources\\property.property");
-        System.out.println(PropertyReaderToMap.propertyReaderToMap("1"));
-        System.out.println(PropertyReaderToMap.propertyReaderToMap("3"));
+        PropertyReaderToMap propertyReaderToMap = new PropertyReaderToMap();
+        propertyReaderToMap.propertyRead("javase06\\src\\main\\java\\t02\\resources\\property.property");
+        propertyReaderToMap.propertyReaderToMap();
+        System.out.println(propertyReaderToMap.propertyStringRead("3"));
+        System.out.println(propertyReaderToMap.propertyStringRead("1"));
 
         //При добавлении элемента с ключом, который уже присутствует, прошлый ключ перезаписывается
     }
